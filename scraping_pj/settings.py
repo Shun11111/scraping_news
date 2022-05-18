5ba1fe7fc21839aaa4ca2c2329b362c6a7c99c60
showtech-prog <67813131+Shun11111@users.noreply.github.com>
2022-05-18 11:28:37 +0900

    aws用に修正
    aws用に修正

--- scraping_pj/settings.py
+++ scraping_pj/settings.py
@@ -82,13 +82,15 @@ WSGI_APPLICATION = 'scraping_pj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(),
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
