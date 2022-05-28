e9367fa923115e78470b1037fc7736ae60e9d5a8
showtech-prog <67813131+Shun11111@users.noreply.github.com>
2022-05-28 09:38:29 +0900

    fix

--- scraping_pj/urls.py
+++ scraping_pj/urls.py
@@ -16,11 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include 
-from django.conf.urls import url, static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('scraping_app.urls')),
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
